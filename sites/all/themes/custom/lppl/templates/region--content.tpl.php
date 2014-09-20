8fbf1855ac59f9016369a713b11f886122a44121
yvandouenel <yvan@coopernet.fr>
2014-09-20 11:29:24 +0200

    region--content.tpl.php modified.

--- sites/all/themes/custom/lppl/templates/region--content.tpl.php
+++ sites/all/themes/custom/lppl/templates/region--content.tpl.php
@@ -4,7 +4,7 @@
     <?php print render($title_prefix); ?>
     <?php if ($title): ?>
     <?php if ($title_hidden): ?><div class="element-invisible"><?php endif; ?>
-    <h1 class="title" id="page-title"><?php print $title; ?></h1>
+    <h2 class="title" id="page-title"><?php print $title; ?></h2>
     <?php if ($title_hidden): ?></div><?php endif; ?>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
