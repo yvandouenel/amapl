01e5ced83c0e3647214fac2006723018d9b8aa62
Yvan Douenel <y.douenel@coopernet.fr>
2016-02-18 17:15:31 +0100

    template.php up.

--- sites/bdpl.fr/themes/custom/vigipl/less/template.php
+++ sites/bdpl.fr/themes/custom/vigipl/less/template.php
@@ -3,3 +3,11 @@
  * @file
  * The primary PHP file for this theme.
  */
+function vigipl_preprocess_html(&$vars, $hook) {
+  if ($vars['user']) {
+    foreach($vars['user']->roles as $key => $role){
+      $role_class = 'role-' . drupal_clean_css_identifier($role);
+      $vars['classes_array'][] = $role_class;
+    }
+  }
+}
