479e2e5d9480fe2c527971098a250a6fc35b7c1e
Yvan Douenel <y.douenel@coopernet.fr>
2016-03-31 12:15:45 +0200

    html.tpl.php updated for tdnim to activate search console.

--- sites/tdnim.com/themes/creative-responsive-theme/templates/html.tpl.php
+++ sites/tdnim.com/themes/creative-responsive-theme/templates/html.tpl.php
@@ -5,6 +5,7 @@
 <?php print $styles; ?>
 <?php print $scripts; ?>
 <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <meta name="google-site-verification" content="SlPZFAUoIqS-jj7tRG6AmcEUgV6Ca6XGSC2FwUCN1vc" />
 </head>
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print $page_top; ?>
