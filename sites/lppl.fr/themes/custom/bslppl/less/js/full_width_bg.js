06af249f5406086c5d1b5bd89b34601bcdf12834
Yvan Douënel <y.douenel@coopernet.fr>
2016-11-18 17:45:25 +0100

    LPPL bootstrap theme added.

---
+++ sites/lppl.fr/themes/custom/bslppl/less/js/full_width_bg.js
@@ -0,0 +1,23 @@
+( function($) {
+
+    $(window).on('resize', ppaFullWidth);
+
+    function ppaFullWidth() {
+        var $elements = $('.full-width-bg');//[data-ppa-full-width="true"]
+        $.each( $elements, function( key, item ) {
+            var $el = $(this);
+            var $container = $el.closest('.container');
+            var margin_left_right = ($(window).width() - $container.width()) / 2;
+
+            $el.css({
+                position: "relative",
+                left: -margin_left_right - 15 + "px",
+                "box-sizing": "border-box",
+                width: $(window).width(),
+                "padding-left": margin_left_right + 15 + "px",
+                "padding-right": margin_left_right + 15 + "px"
+            });
+        });
+    }
+    ppaFullWidth();
+} ) ( jQuery );
