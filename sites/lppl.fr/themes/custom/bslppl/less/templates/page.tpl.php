573e06595fb3a1db8fcbc737a8e97737ad18df0c
Yvan Douënel <y.douenel@coopernet.fr>
2016-11-21 17:29:23 +0100

    page.tpl.php up.

--- sites/lppl.fr/themes/custom/bslppl/less/templates/page.tpl.php
+++ sites/lppl.fr/themes/custom/bslppl/less/templates/page.tpl.php
@@ -79,44 +79,45 @@
       <?php print render($page['header']); ?>
     </div> <!-- /#page-header -->
   </div>
+  <div class="custom-headband">
+    <div class="<?php print $container_class; ?>">
+      <div class="navbar-header">
+        <?php if ($logo): ?>
+          <h1 id="h1-logo">
+            <a class="logo navbar-btn " href="<?php print $front_page; ?>" >
+              <img src="<?php print $logo; ?>" alt="lppl.fr, le portail des professions libérales - retour accueil" />
+            </a>
+          </h1>
+        <?php endif; ?>
 
-  <div class="<?php print $container_class; ?>">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
-        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      <?php endif; ?>
+        <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        <?php endif; ?>
+      </div>
 
-      <?php if (!empty($site_name)): ?>
-        <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
 
       <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
+        <div class="navbar-collapse collapse">
+          <nav role="navigation">
+            <?php if (!empty($primary_nav)): ?>
+              <?php print render($primary_nav); ?>
+            <?php endif; ?>
+            <!-- <?php if (!empty($secondary_nav)): ?>
+              <?php print render($secondary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($page['navigation'])): ?>
+              <?php print render($page['navigation']); ?>
+            <?php endif; ?>
+            -->
+          </nav>
+        </div>
       <?php endif; ?>
     </div>
-
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
-      </div>
-    <?php endif; ?>
   </div>
 </header>
 
@@ -166,7 +167,10 @@
 </div>
 
 <?php if (!empty($page['footer'])): ?>
-  <footer class="footer <?php print $container_class; ?>">
-    <?php print render($page['footer']); ?>
-  </footer>
+  <div id="custom-footer">
+    <footer class="footer <?php print $container_class; ?>">
+      <a href="/constellians" id="link-constellians"><img src="/sites/lppl.fr/themes/custom/bslppl/less/images/constellians-bas.png" alt="Constellians - Groupement d'Intérêt Economique d'Associations de Gestions Agréées" /></a>
+      <?php print render($page['footer']); ?>
+    </footer>
+  </div>
 <?php endif; ?>
