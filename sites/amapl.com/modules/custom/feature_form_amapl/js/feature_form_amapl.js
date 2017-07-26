07fbe897b8f0bb24f7923770a5a6086704293830
Yvan Douenel <y.douenel@coopernet.fr>
2017-07-26 15:13:13 +0200

    Feature feature_form_amapl up.

--- sites/amapl.com/modules/custom/feature_form_amapl/js/feature_form_amapl.js
+++ sites/amapl.com/modules/custom/feature_form_amapl/js/feature_form_amapl.js
@@ -37,6 +37,16 @@
         formeJuridique();
         manageFields();
       });
+      // Type d'adhésion
+      $("#edit-field-type-adhesion-und input").change(function () {
+        manageFields();
+      });
+      // Assistance d'un pro ?
+      $("#edit-field-assistance input").change(function () {
+        manageFields();
+      });
+
+
       /* Fin de gestion des événements ********************************************************************************/
       function formeJuridique() {
         // Société unipersonnelle ?
@@ -56,8 +66,9 @@
       }
 
       function manageFields() {
-        console.log("Societe : " + societe);
-        console.log("societe_unipersonnelle : " + societe_unipersonnelle);
+        /*console.log("Societe : " + societe);
+        console.log("societe_unipersonnelle : " + societe_unipersonnelle);*/
+
         if (!$("#edit-field-forme-juridique-und-autre").attr('checked')) {
           $("#field-fj-autre-add-more-wrapper").hide();
         }
@@ -69,6 +80,9 @@
           professionnal = true;
           $("#edit-field-non-pro").hide().find("input").attr('checked', false);
           $("#field-fj-autre-add-more-wrapper").hide();
+          $("#edit-field-siret").show();
+          console.log($("#edit-field-siret").val());
+          if ($("#edit-field-siret-und-0-value").val() == "Pas de siret") $("#edit-field-siret-und-0-value").val('');
         } else {
           $("#field-fj-autre-add-more-wrapper").show();
           $("#edit-field-non-pro").show();
@@ -77,8 +91,6 @@
           }else{
             $("#edit-field-siret").show().find("input").val("");
           }
-
-
         }
         if (societe_unipersonnelle) {
           $("#field-nombre-associes-add-more-wrapper").hide();
@@ -91,6 +103,21 @@
         else {
           $("#field-nombre-associes-add-more-wrapper").show();
           $("#edit-field-micro-autoentrepreneur").show()
+        }
+        if (!$("#edit-field-type-adhesion-und-autre").is(":checked")) {
+          $("#edit-field-autre-type-adhesion").hide();
+        } else {
+          $("#edit-field-autre-type-adhesion").show();
+        }
+        if (!$("#edit-field-type-adhesion-und-transfert").is(":checked")) {
+          $("#edit-field-autre-aga").hide();
+        } else {
+          $("#edit-field-autre-aga").show();
+        }
+        if ($("#edit-field-assistance-und-non").is(":checked")) {
+          $(".group-tenue .field-type-text").hide();
+        } else {
+          $(".group-tenue .field-type-text").show();
         }
 
       }
@@ -133,105 +160,3 @@
     }
   };
 }(jQuery))
-
-/*
- // ----------- TECHNICAL FIELDS HANDLING -------------------------------------------------------------------------
- // We hide field Action
- $('#edit-field-need-action').hide();
-
- // ----------- SOLUTION FIELDS HANDLIGN --------------------------------------------------------------------------
- // First, we get the value of radio button field_solution_found (no = 0, yes = 1).
- var initialSolutionFoundValue = $('#edit-field-solution-found input[type=radio]:checked').val();
- // If 'no', we must hide field_solution_details.
- if (initialSolutionFoundValue == 0) {
- $('#edit-field-solution-details').hide();
- }
- // Then, we handle radio button change.
- $('#edit-field-solution-found input[type=radio]').change(function () {
- if ($('#edit-field-solution-found input[type=radio]:checked').val() == 1) {
- $('#edit-field-solution-details').show(1000);
- }
- else {
- $('#edit-field-solution-details').hide(1000);
- }
- });
-
- var client_uuids = settings.crealead_needs_clients;
-
- var initialCrealeadName = $('#edit-field-crealead-name-und-0-value').val();
- var initialContactName = $('#edit-field-need-contact-name-und-0-value').val();
- var initialContactDetails = $('#edit-field-need-contact-details-und-0-value').val();
-
- var initialClientValue = $('#edit-field-need-client-und').val();
-
- if (initialClientValue == '_none') {
- $('#edit-field-crealead-name').hide();
- $('.group-need-contact-infos').hide();
- }
- else {
- if (client_uuids[initialClientValue] == '7b581fb4-6fcb-4be4-8f14-53f14dc4f3d4') { // Client externe.
- $('#edit-field-crealead-name').hide();
- }
- else if (client_uuids[initialClientValue] == '7a01c453-5ac3-42a6-a374-ee9d26137a2f') { // Crealead Structure
- $('.group-need-contact-infos').hide();
- }
- }
-
- // Client field change handling.
- $('#edit-field-need-client-und').change(function() {
- var selectedClientValue = $('#edit-field-need-client-und option:selected').val();
-
- // 1 - Fields content handling
- if (selectedClientValue == '_none') {
- // _none value is available only on Creation mode, so we can empty all client fields values.
- $('#edit-field-crealead-name-und-0-value').val('');
- $('#edit-field-need-contact-name-und-0-value').val('');
- $('#edit-field-need-contact-details-und-0-value').val('');
- }
- else {
- var selectedClientUuid = client_uuids[selectedClientValue];
- var connectedUser = settings.crealead_needs_connected_user;
-
- if (selectedClientValue == initialClientValue) {
- $('#edit-field-crealead-name-und-0-value').val(initialCrealeadName);
- $('#edit-field-need-contact-name-und-0-value').val(initialContactName);
- $('#edit-field-need-contact-details-und-0-value').val(initialContactDetails);
- }
- else {
- $('#edit-field-crealead-name-und-0-value').val(connectedUser.name);
- $('#edit-field-need-contact-name-und-0-value').val(connectedUser.name);
- $('#edit-field-need-contact-details-und-0-value').val(connectedUser.mail);
-
- if (selectedClientUuid == '7a01c453-5ac3-42a6-a374-ee9d26137a2f') { // Crealead Structure
- $('#edit-field-need-contact-name-und-0-value').val('');
- $('#edit-field-need-contact-details-und-0-value').val('');
- }
- else if (selectedClientUuid == '7b581fb4-6fcb-4be4-8f14-53f14dc4f3d4') { // Client externe
- $('#edit-field-crealead-name-und-0-value').val('');
- $('#edit-field-need-contact-name-und-0-value').val('');
- $('#edit-field-need-contact-details-und-0-value').val('');
- }
- }
- }
-
- // 2 - Fields visibility handling
- if (selectedClientValue == '_none') {
- $('#edit-field-crealead-name').hide(500);
- $('.group-need-contact-infos').hide(500);
- }
- else if (selectedClientUuid == '7b581fb4-6fcb-4be4-8f14-53f14dc4f3d4') { // Client externe
- $('#edit-field-crealead-name').hide(250);
- $('.group-need-contact-infos').show(500);
- }
- else if (selectedClientUuid == '7a01c453-5ac3-42a6-a374-ee9d26137a2f') { // Crealead Structure
- $('#edit-field-crealead-name').show(500);
- $('.group-need-contact-infos').hide(250);
- }
- else  {
- $('#edit-field-crealead-name').show(500);
- $('.group-need-contact-infos').show(500);
- }
- });
-
- ;
- */
