ccd8a8c25619514a744aa7343d5bc7707beab9dd
Yvan Douenel <y.douenel@coopernet.fr>
2018-10-30 12:05:44 +0100

    amapl_training_session_popup.js up to manage mandatory fields in amapl registration.

--- sites/amapl.com/modules/custom/amapl_trainings/js/amapl_training_session_popup.js
+++ sites/amapl.com/modules/custom/amapl_trainings/js/amapl_training_session_popup.js
@@ -2,11 +2,25 @@
  * Created by ubuntu on 13/10/17.
  */
 
-
 (function ($) {
 
   Drupal.behaviors.trainingSessionPopup = {
     attach: function (context, settings) {
+
+      $("input.form-submit").click(function(event){
+        if($("#edit-field-amapl-form-rules-und--2:checked").length < 1 ||
+          !$(".required").val()) {
+          event.preventDefault();
+          if (!$("#msg-error-registration-amapl").length) {
+            $("<div></div>",{
+              "id": "msg-error-registration-amapl",
+              "text": "Vous devez remplir tous les champs précédés d'un astérisque rouge.",
+              "style": "color:red",
+            }).insertAfter(".field-name-field-amapl-form-rules");
+          }
+        }
+
+      });
       $('.add-to-session-open-close').click(function () {
         $(this).hide();
         $('#session-switch-wrapper .user-details').slideDown(1000);
