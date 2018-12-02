4354253b6a58c00d9083460788133b1ab61988f6
Yvan Douenel <y.douenel@coopernet.fr>
2018-12-02 21:24:26 +0100

    Template.php up to call sameHeight js.

--- sites/bdpl.fr/themes/custom/vigipl/less/template.php
+++ sites/bdpl.fr/themes/custom/vigipl/less/template.php
@@ -11,3 +11,9 @@ function vigipl_preprocess_html(&$vars, $hook) {
     }
   }
 }
+function vigipl_preprocess_page(&$vars) {
+  if(drupal_is_front_page()) {
+    drupal_add_js(drupal_get_path('theme', 'vigipl') . '/js/jquery.matchHeight-min.js');
+    drupal_add_js(drupal_get_path('theme', 'vigipl') . '/js/match-actu-height.js');
+  }
+}
