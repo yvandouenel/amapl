570466428096d027897fac4a68b903789d4bbdd8
yvan <y.douenel@coopernet.fr>
2019-07-12 17:59:41 +0200

    template.php modify to call js on homepage.

--- sites/amapl.com/themes/custom/bootamapl/less/template.php
+++ sites/amapl.com/themes/custom/bootamapl/less/template.php
@@ -3,3 +3,8 @@
  * @file
  * The primary PHP file for this theme.
  */
+function bootamapl_preprocess_page(&$vars) {
+  if(drupal_is_front_page()) {
+    drupal_add_js(drupal_get_path('theme', 'bootamapl') . '/js/homme_ss_images.js');
+  }
+}
