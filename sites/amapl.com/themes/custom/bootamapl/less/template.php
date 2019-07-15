71e0d51d539701a93b432f1743064925939056d3
yvan <y.douenel@coopernet.fr>
2019-07-15 18:05:46 +0200

    template.php up to manage tpl name. Ex node--news--teaser.tpl.php

--- sites/amapl.com/themes/custom/bootamapl/less/template.php
+++ sites/amapl.com/themes/custom/bootamapl/less/template.php
@@ -8,3 +8,13 @@ function bootamapl_preprocess_page(&$vars) {
     drupal_add_js(drupal_get_path('theme', 'bootamapl') . '/js/homme_ss_images.js');
   }
 }
+/**
+ * Permet de choisir un nom de theme pour un display ou un node donné
+ * That will let you use a template file like: node--[type|nodeid]--teaser.tpl.php
+ */
+function bootamapl_preprocess_node(&$vars) {
+  if($vars['view_mode'] == 'teaser') {
+    $vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->type . '__teaser';
+    $vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->nid . '__teaser';
+  }
+}
