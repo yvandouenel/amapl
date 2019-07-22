7b85007b1da631abbe53295f9446c6a324b8be31
yvan <y.douenel@coopernet.fr>
2019-07-22 18:15:48 +0200

    page.tpl.php up.

--- sites/amapl.com/themes/custom/bootamapl/less/templates/page.tpl.php
+++ sites/amapl.com/themes/custom/bootamapl/less/templates/page.tpl.php
@@ -116,6 +116,17 @@
 
   </div>
 </header>
+<?php if (!empty($title)): ?>
+  <div id="under-main-title">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="page-header"><?php print $title; ?></h2>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php endif; ?>
 <div id="under-main-container">
   <div class="main-container <?php print $container_class; ?>">
 
@@ -167,12 +178,10 @@
         <?php if (!empty($page['highlighted'])): ?>
           <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
         <?php endif; ?>
-        <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+
         <a id="main-content"></a>
         <?php print render($title_prefix); ?>
-        <?php if (!empty($title)): ?>
-          <h1 class="page-header"><?php print $title; ?></h1>
-        <?php endif; ?>
+
         <?php print render($title_suffix); ?>
         <?php print $messages; ?>
         <?php if (!empty($tabs)): ?>
