b8b9fa11925efda3abcac312b9317676c37287f6
yvan <y.douenel@coopernet.fr>
2019-07-24 19:20:37 +0200

    template.php up.

--- sites/amapl.com/themes/custom/bootamapl/less/template.php
+++ sites/amapl.com/themes/custom/bootamapl/less/template.php
@@ -3,10 +3,15 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+/**
+ * Appel du fichier js /js/homme_ss_images.js depuis la page d'accueil
+ */
 function bootamapl_preprocess_page(&$vars) {
   if(drupal_is_front_page()) {
     drupal_add_js(drupal_get_path('theme', 'bootamapl') . '/js/homme_ss_images.js');
   }
+  drupal_add_js(drupal_get_path('theme', 'bootamapl') . '/js/drop-down-para.js');
 }
 /**
  * Permet de choisir un nom de theme pour un display ou un node donné
