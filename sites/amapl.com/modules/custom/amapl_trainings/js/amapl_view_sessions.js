af45520e9bf1e54787c12db465d54baeba938b38
yvan <y.douenel@coopernet.fr>
2019-07-26 13:08:46 +0200

    amapl_view_sessions.js up.

--- sites/amapl.com/modules/custom/amapl_trainings/js/amapl_view_sessions.js
+++ sites/amapl.com/modules/custom/amapl_trainings/js/amapl_view_sessions.js
@@ -1,25 +1,42 @@
-(function ($) {
+(function($) {
   Drupal.behaviors.trainingFull = {
-    attach: function (context, settings) {
-     var free_checked = ($("#edit-field-training-free-value-1").is(':checked')) ? true : false;
-     if($("#edit-field-training-free-value-0").is(':checked')) {
-       $("#edit-field-training-free-value-all").prop("checked", true);
-     }
-      $(".form-item-field-training-free-value").each(function (index) {
-        if (index == 0 || index == 1) {
-          $(this).hide();
-        } else {
-          $("label",$(this)).hide();
-        }
-      });
-      $("#edit-field-training-free-value-1").click(function() {
-        if (free_checked) {
-          $("#edit-field-training-free-value-all").prop("checked", true);
-          free_checked = false;
-        } else {
-          free_checked = true;
-        }
-      });
+    attach: function(context, settings) {
+      // on efface tous les labels des boutons radios
+      $(".form-item-field-training-free-value > label").next().text("");
+      let free_checked = $("#edit-field-training-free-value-1").is(":checked")
+        ? true
+        : false;
+
+      /**
+       * si "gratuit" est coché, on ne laisse que "tout"
+       * et on remet le label "Gratuit et payant ?"
+      */
+      if (free_checked) {
+        //console.log("gratuit coché");
+        $(".form-item-field-training-free-value").each(function(index) {
+          if (index == 1 || index == 2) {
+            $(this).hide();
+          }
+        });
+        $("#edit-field-training-free-value-wrapper > label").text("Gratuit et payant ?");
+      }
+      /**
+       * si "tout" est coché, on ne laisse que "free"
+       * et on remet le label "gratuit ?"
+       */
+      else if ($("#edit-field-training-free-value-0").is(":checked")) {
+        //console.log("Tout coché");
+        // Attention : important de cocher "all" sinon, la recherche est erronnée
+        $("#edit-field-training-free-value-all").prop("checked", true);
+        $(".form-item-field-training-free-value").each(function(index) {
+          if (index == 0 || index == 1) {
+            $(this).hide();
+          }
+        });
+        $("#edit-field-training-free-value-wrapper > label").text("Gratuit ?");
+
+      }
+
     }
   };
-}(jQuery));
+})(jQuery);
