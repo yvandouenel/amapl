5c0c021ca9a1e72e95c811bd5a79a770d7a6aa85
yvan <y.douenel@coopernet.fr>
2019-07-29 18:26:04 +0200

    template.php up to manage simple-title.

--- sites/amapl.com/themes/custom/bootamapl/less/template.php
+++ sites/amapl.com/themes/custom/bootamapl/less/template.php
@@ -8,11 +8,30 @@
  * Appel du fichier js /js/homme_ss_images.js depuis la page d'accueil
  */
 function bootamapl_preprocess_page(&$vars) {
+  /**
+   * Gestion de la classe du titre principal
+  */
+  $vars['title_class'] = "complex-title";
+  if (views_get_page_view() && views_get_page_view()->name == "files" ){
+    $vars['title_class'] = "simple-title";
+  }
+  else if (views_get_page_view() && views_get_page_view()->name == "lettres_info" ){
+    $vars['title_class'] = "simple-title";
+  }
+  else if (isset($vars['node']) &&
+    isset($vars['node']->field_titre_simple_['und'][0]['value']) &&
+    $vars['node']->field_titre_simple_['und'][0]['value']) {
+      $vars['title_class'] = "simple-title";
+  }
+
+  // Gestion du slideshow d'accueil
   if(drupal_is_front_page()) {
     drupal_add_js(drupal_get_path('theme', 'bootamapl') . '/js/homme_ss_images.js');
   }
+  // Gestion des paragraphes qui s'ouvrent et se ferment au click sur un titre
   drupal_add_js(drupal_get_path('theme', 'bootamapl') . '/js/drop-down-para.js');
 }
+
 /**
  * Permet de choisir un nom de theme pour un display ou un node donné
  * That will let you use a template file like: node--[type|nodeid]--teaser.tpl.php
