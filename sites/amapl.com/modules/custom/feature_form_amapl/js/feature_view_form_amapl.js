9caa57948692c46e9f6f987ec74a7138af91aadf
yvan <y.douenel@coopernet.fr>
2019-08-02 17:35:07 +0200

    feature_view_form_amapl.js up.

--- sites/amapl.com/modules/custom/feature_form_amapl/js/feature_view_form_amapl.js
+++ sites/amapl.com/modules/custom/feature_form_amapl/js/feature_view_form_amapl.js
@@ -7,7 +7,10 @@
           id: "modify-subscribe-form",
           text: "> Modifier mes informations avant paiement",
           href: "/adherer/formulaire-bic-ba"
-        }).appendTo(".group-right");
+        }).prependTo(".group-left");
+        $("#under-main-title").addClass("caa-title");
+        $("h2.page-header").addClass("caa-title");
+        $("body").addClass("caa-form");
       } else {
         // spécifique aux PL *****************************************
         $("<a></a>", {
@@ -15,7 +18,23 @@
           text: "> Modifier mes informations avant paiement",
           href: "/adherer/formulaire"
         }).appendTo(".group-right");
+        $("#under-main-title").addClass("pl-title");
+        $("h2.page-header").addClass("pl-title");
       }
+
+      // changement d'intitulé du titre
+      $("h2.page-header").text("Payer et voir le " + $("h2.page-header").text());
+
+      // On cache les infos
+      $(".group-left, .group-right").slideToggle(400);
+
+      // on gère la visualisation des données
+      $("<h3></h3>", {
+        id: "title-show-hide-form-datas",
+        text: "Vérifier les données que je viens de communiquer",
+      }).insertAfter(".group-header").click(function(){
+        $(".group-left, .group-right").slideToggle(400);
+      });
 
       console.log("Dans feature_view_form_amapl.js");
       $("<h3></h3>", {
