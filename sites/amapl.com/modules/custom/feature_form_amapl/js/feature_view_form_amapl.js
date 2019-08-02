5079abc572479c3ecad1322d24bb72372903d62c
yvan <y.douenel@coopernet.fr>
2019-08-02 16:19:57 +0200

    Feature feature_view_form_amapl up.

--- sites/amapl.com/modules/custom/feature_form_amapl/js/feature_view_form_amapl.js
+++ sites/amapl.com/modules/custom/feature_form_amapl/js/feature_view_form_amapl.js
@@ -1,38 +1,38 @@
-(function ($) {
+(function($) {
   Drupal.behaviors.form_amapl = {
-    attach: function (context, settings) {
-      $( "<h3></h3>", {
-        "id": "h3-payment-online",
-        text: 'Paiement en ligne :',
-
-      }).prependTo( ".group-footer" );
-
-      /*$( "<h3></h3>", {
-        "id": "h3-subscribed-datas",
-        text: 'Résumé des principales informations que vous nous avez communiquées :',
-
-      }).appendTo( ".group-header" );*/
-
-      $( "<a></a>", {
-        "id": "modify-subscribe-form",
-        text: "> Modifier mes informations avant paiement",
-        "href": "/adherer/formulaire",
-      }).appendTo( ".group-right" );
-
-
-      if ($('#title-payment-history').length) {
-        $("#payment-form-standalone").hide();
-        $( "<button></button>", {
-          "id": "view-payment",
-          text: 'Effecturer un nouveau payement',
-          click: function( event ) {
-              console.log("qsdqsdfqsdaa")
-              $("#payment-form-standalone").show();
-            }
-
-        }).insertAfter( ".field-name-field-historique-paiement" );
+    attach: function(context, settings) {
+      // spécifique aux CAA *******************************************
+      if ($(".field-name-field-date-du").length) {
+        $("<a></a>", {
+          id: "modify-subscribe-form",
+          text: "> Modifier mes informations avant paiement",
+          href: "/adherer/formulaire-bic-ba"
+        }).appendTo(".group-right");
+      } else {
+        // spécifique aux PL *****************************************
+        $("<a></a>", {
+          id: "modify-subscribe-form",
+          text: "> Modifier mes informations avant paiement",
+          href: "/adherer/formulaire"
+        }).appendTo(".group-right");
       }
 
+      console.log("Dans feature_view_form_amapl.js");
+      $("<h3></h3>", {
+        id: "h3-payment-online",
+        text: "Paiement en ligne :"
+      }).prependTo(".group-footer");
+
+      if ($("#title-payment-history").length) {
+        $("#payment-form-standalone").hide();
+        $("<button></button>", {
+          id: "view-payment",
+          text: "Effectuer un nouveau payement",
+          click: function(event) {
+            $("#payment-form-standalone").show();
+          }
+        }).insertAfter(".field-name-field-historique-paiement");
+      }
     }
   };
-}(jQuery))
+})(jQuery);
