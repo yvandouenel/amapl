bad37b4c37949b46d11687ff98487ab7a8cb96f1
yvandouenel <y.douenel@coopernet.fr>
2021-04-13 12:04:27 +0200

    amapl_view_trainings.js up sameHeight not working

--- sites/amapl.com/modules/custom/amapl_trainings/js/amapl_view_trainings.js
+++ sites/amapl.com/modules/custom/amapl_trainings/js/amapl_view_trainings.js
@@ -1,9 +1,9 @@
 jQuery(function($) {
-  $('.group-training-data > div').matchHeight();
+  /*$('.group-training-data > div').matchHeight();
   $('.page-formations .group-sub-wrapper-teaser').matchHeight();
   $('.page-formations .group-wrapper-teaser').matchHeight();
   $( document ).ajaxComplete(function() {
     $('.group-training-data > div').matchHeight();
     $('.page-formations .group-wrapper-teaser').matchHeight();
-  });
+  });*/
 });
