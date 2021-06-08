b234feafdaf356aafd3e56fd6b5054b17193a6ed
yvandouenel <y.douenel@coopernet.fr>
2021-06-08 18:24:51 +0200

    google tag manager added to tdnim.

--- sites/tdnim.com/themes/boottdnim/less/templates/html.tpl.php
+++ sites/tdnim.com/themes/boottdnim/less/templates/html.tpl.php
@@ -56,6 +56,13 @@
 ?><!DOCTYPE html>
 <html<?php print $html_attributes;?><?php print $rdf_namespaces;?>>
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WMP2RB4');</script>
+<!-- End Google Tag Manager -->
   <link rel="profile" href="<?php print $grddl_profile; ?>" />
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -68,6 +75,7 @@
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
   <?php print $scripts; ?>
+  
 </head>
 <body<?php print $body_attributes; ?>>
   <div id="skip-link">
@@ -76,5 +84,9 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WMP2RB4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 </body>
 </html>
