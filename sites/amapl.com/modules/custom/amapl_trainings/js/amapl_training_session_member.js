204da7adc0d8ea5c545ce44ade735093df4e6526
yvandouenel <y.douenel@coopernet.fr>
2022-01-14 15:26:03 +0100

    Feature amapl_trainings/js/amapl_training_session_member.js up to manage anonymous user.

--- sites/amapl.com/modules/custom/amapl_trainings/js/amapl_training_session_member.js
+++ sites/amapl.com/modules/custom/amapl_trainings/js/amapl_training_session_member.js
@@ -3,30 +3,33 @@
  */
 
 (function ($) {
-
+  let onlyOnce;
   Drupal.behaviors.trainingSessionMember = {
     attach: function (context, settings) {
-      console.log(` context : `, context);
-      if (context !== "#cboxLoadedContent") {
-        return;
-      }
-      // Afficher / cacher l'input du n° d'adhérent
-      const checkbox_member = $("#edit-field-amapl-member-und--2");
-      checkbox_member.on("change",function(event) {
-        console.log(`event.target : `, event.target.checked);
-        if(event.target.checked) {
-          $("#edit-field-amapl-form-member-und-0-value--2").val("");
-          $("#field-amapl-form-member-add-more-wrapper--2").show("1000");
-        } else {
-          $("#edit-field-amapl-form-member-und-0-value--2").val("Non adhérent");
-          $("#field-amapl-form-member-add-more-wrapper--2").hide("1000");
-        }
-      });
 
-      // Rendre obligatoire le mail dans le cas où la personne n'est pas identifiée
-      $("#edit-anon-mail--2").attr("required", "true");
-      const asterisk = $(`<span class="form-required" title="Ce champ est requis.">*</span>`);
-      asterisk.appendTo(".form-item-anon-mail label");
+
+        console.log(` context dans  trainingSessionMember :: `, context);
+
+        // Afficher / cacher l'input du n° d'adhérent
+
+        const checkbox_member = $("#edit-field-amapl-member-und--2");
+        checkbox_member.on("change",function(event) {
+          console.log(`event.target : `, event.target.checked);
+          if(event.target.checked) {
+            $("#edit-field-amapl-form-member-und-0-value--2").val("");
+            $("#field-amapl-form-member-add-more-wrapper--2").show("1000");
+          } else {
+            $("#edit-field-amapl-form-member-und-0-value--2").val("Non adhérent");
+            $("#field-amapl-form-member-add-more-wrapper--2").hide("1000");
+          }
+        });
+      if (!onlyOnce &&  $("#edit-anon-mail--2").length) {
+        onlyOnce = true;
+        // Rendre obligatoire le mail dans le cas où la personne n'est pas identifiée
+        $("#edit-anon-mail--2").attr("required", "true");
+        const asterisk = $(`<span class="form-required" title="Ce champ est requis.">*</span>`);
+        asterisk.appendTo(".form-item-anon-mail label");
+      }
 
     }
   };
